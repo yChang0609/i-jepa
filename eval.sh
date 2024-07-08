4090a2fbbad827bc342290f43a251ce38a9b2735
yChang0609 <ychang0609@gmail.com>
2024-07-08 15:31:26 +0800

    train script

--- eval.sh
+++ eval.sh
@@ -1,2 +1,20 @@
 # python3 main_probing.py --fname logs/in-tiny_vitc_tiny-8_ep20 --devices cuda:0 
-python3 main_vit.py --fname configs/Experiment/IN-tiny/in-tiny_vitc_tiny-8_ep20.yaml --devices cuda:0 
+python3 main_vit.py --fname configs/Experiment/IN-tiny/in-tiny_vit_base-8_ep20.yaml --devices cuda:0 
+python3 main_vit.py --fname configs/Experiment/IN-tiny/in-tiny_vitc_base-8_ep20.yaml --devices cuda:0 
+python3 main_vit.py --fname configs/Experiment/IN-tiny/in-tiny_vit_huge-8_ep20.yaml --devices cuda:0 
+python3 main_vit.py --fname configs/Experiment/IN-tiny/in-tiny_vitc_huge-8_ep20.yaml --devices cuda:0 
+
+python3 main_probing.py --fname logs/in-tiny_vit_base-8_ep20 --devices cuda:0 
+python3 main_probing.py --fname logs/in-tiny_vitc_base-8_ep20 --devices cuda:0 
+python3 main_probing.py --fname logs/in-tiny_vit_huge-8_ep20 --devices cuda:0 
+python3 main_probing.py --fname logs/in-tiny_vitc_huge-8_ep20-v2 --devices cuda:0  
+
+# ViT-Base	            
+# ViTC-Base-v1	        
+# ViT-Huge	          
+# ViTC-Huge-v2	      
+
+# JEPA-Base-LB	        
+# JEPA(C)-Base-v1-LB	
+# JEPA-Huge-LB	        
+# JEPA(C)-Huge-v2-LB	
