40b78619c783e47ee0b1fbf37a125acee1f6f17d
yChang <ychang0609@gmail.com>
2024-07-21 21:54:57 +0800

    train select and template of yaml

--- run_script.sh
+++ run_script.sh
@@ -7,12 +7,18 @@ do
     if [ -f "$file" ]; then
         echo "run config:$file"
         logfolder=$(yq .logging.folder $file)
-        echo "Training vit classification"
-        python3 main_vit.py --fname "$file" --devices cuda:0  
-        echo "Training I-JEPA"
-        python3 main.py --fname "$file" --devices cuda:0
-        echo "Evaluate I-JEPA"
-        python3 main_probing.py --fname $MOUNT_PATH/${logfolder//\"/} --devices cuda:0
+        train_vit_cls=$(yq '.train_type.vit_cls' $file)
+        train_jepa_cls=$(yq '.train_type.jepa_cls' $file)
+        if $train_vit_cls ;then
+            echo "Training vit classification"
+            python3 main_vit.py --fname "$file" --devices cuda:0  
+        fi
+        if $train_jepa_cls ;then
+            echo "Training I-JEPA"
+            python3 main.py --fname "$file" --devices cuda:0
+            echo "Evaluate I-JEPA"
+            python3 main_probing.py --fname $MOUNT_PATH/${logfolder//\"/} --devices cuda:0
+        fi
     fi
 done
 echo "Script end"
