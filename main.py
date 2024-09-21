09dda5b0191f17feae1b4f34631e5b7496ad5691
yChang0609 <ychang0609@gmail.com>
2024-09-21 16:44:11 +0800

    organize main.py code add parameter contorl

--- main.py
+++ main.py
@@ -13,9 +13,24 @@ import pprint
 import yaml
 
 from src.utils.distributed import init_distributed
-from src.train import main as app_main
+
+
+class TrainMode:
+    jepa = "jepa"
+    jepa_linear_prob = "jepa_linear_prob"
+    vit_cls = "vit_cls"
+    vae = "vae"
 
 parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--train', type=str,
+    help=f'decision training mode[
+    {TrainMode.jepa}, 
+    {TrainMode.jepa_linear_prob}, 
+    {TrainMode.vit_cls}, 
+    {TrainMode.vae}]',
+    default=TrainMode.jepa)
+
 parser.add_argument(
     '--fname', type=str,
     help='name of config file to load',
@@ -25,10 +40,12 @@ parser.add_argument(
     help='which devices to use on local machine')
 
 
-def process_main(rank, fname, world_size, devices):
+def process_main(rank, train_mode, fname, world_size, devices):
+    # -- enviroment var
     import os
     os.environ['CUDA_VISIBLE_DEVICES'] = str(devices[rank].split(':')[-1])
     mount_path_env = os.getenv('MOUNT_PATH', "./")
+    
     import logging
     logging.basicConfig()
     logger = logging.getLogger()
@@ -40,17 +57,43 @@ def process_main(rank, fname, world_size, devices):
     logger.info(f'called-params {fname}')
 
     # -- load script params
-    params = None
-    with open(fname, 'r') as y_file:
-        params = yaml.load(y_file, Loader=yaml.FullLoader)
-        logger.info('loaded params...')
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(params)
+    if train_mode == TrainMode.jepa or train_mode == TrainMode.vit_cls :    
+        params = None
+        with open(fname, 'r') as y_file:
+            params = yaml.load(y_file, Loader=yaml.FullLoader)
+            logger.info('loaded params...')
+            pp = pprint.PrettyPrinter(indent=4)
+            pp.pprint(params)
+
+    if train_mode == TrainMode.jepa_linear_prob or train_mode == TrainMode.vae :
+        params = None
+        with open(fname, 'r') as y_file:
+            params = yaml.load(y_file, Loader=yaml.FullLoader)
+     
+        log_dir = os.path.join(mount_path_env,params['logging']['folder'])
+        yaml_flie = os.path.join(log_dir,'params-ijepa.yam')
+        with open(yaml_flie, 'r') as y_file:
+            params = yaml.load(y_file, Loader=yaml.FullLoader)
+            logger.info('loaded params...')
+            pp = pprint.PrettyPrinter(indent=4)
+            pp.pprint(params)
 
     world_size, rank = init_distributed(rank_and_world_size=(rank, world_size))
     logger.info(f'Running... (rank: {rank}/{world_size})')
-    app_main(args=params, mount_path=mount_path_env)
-
+    
+    ## --Select work
+    if train_mode == TrainMode.jepa:
+        from src.train import main as jepa_main
+        jepa_main(args=params, mount_path=mount_path_env)
+    elif train_mode == TrainMode.jepa_linear_prob:
+        from src.linear_prob import main as linear_prob_main
+        linear_prob_main(args=params, mount_path=mount_path_env)
+    elif train_mode == TrainMode.vit_cls:
+        from src.vit_trian import main as vit_main
+        vit_main(args=params, mount_path=mount_path_env)
+    elif train_mode == TrainMode.vae:
+        from src.jepa_vae import main as vae_main
+        vae_main(args=params, mount_path=mount_path_env)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -61,5 +104,5 @@ if __name__ == '__main__':
     for rank in range(num_gpus):
         mp.Process(
             target=process_main,
-            args=(rank, args.fname, num_gpus, args.devices)
+            args=(rank, args.train, args.fname, num_gpus, args.devices)
         ).start()
